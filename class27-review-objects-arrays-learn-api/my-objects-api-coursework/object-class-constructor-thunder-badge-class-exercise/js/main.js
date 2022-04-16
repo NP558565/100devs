db688d8e5837da7d9f05aa99130e5724bba705d9
nanji patel <nanji.patel@btinternet.com>
2022-04-16 11:02:23 +0100

    Initial commit 100devs coursework

---
+++ class27-review-objects-arrays-learn-api/my-objects-api-coursework/object-class-constructor-thunder-badge-class-exercise/js/main.js
@@ -0,0 +1,25 @@
+//Objects
+//Please create a pokemon class with as much detail as possible in 10 minutes
+
+class Pokemon {
+    name;
+    type;
+    health;
+    pokePower;
+    attacks;
+    weakness;
+    resistance;
+    retreat;
+    constructor(name, type, health, pokePower, attacks, weakness, resistance, retreat) {
+        this.name = name;
+        this.type = type;
+        this.health = health;
+        this.pokePower = pokePower;
+        this.attacks = attacks;
+        this.weakness = weakness;
+        this.resistance = resistance;
+        this.retreat = retreat;
+    }
+
+
+}
